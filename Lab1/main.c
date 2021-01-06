83e080263068af12099960db557c277acfac772a
wonsukheo <62510983+wonsukheo@users.noreply.github.com>
2021-01-06 15:07:51 -0600

    my first C practice

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,4 +1,14 @@
+#include "add.h"
+#include <stdio.h>
+
 int main(void)
 {
-    return 0;
+	int a = 10;
+	int b = 20;
+	
+	int result = add(a, b);
+	
+	printf("10 + 20 = %d", result);
+   	
+	return 0;
 }
