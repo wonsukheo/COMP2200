7865ee929d55b60f3b5813d748ebba209b92106c
wonsukheo <62510983+wonsukheo@users.noreply.github.com>
2021-01-22 01:48:04 -0600

    no message

--- Lab3/cyber_spaceship.c
+++ Lab3/cyber_spaceship.c
@@ -16,7 +16,7 @@ void update_overlapped_count(const size_t cluster_count, const char* const clust
 
 const char* update_longest_safe_area_length(size_t safe_area_length, size_t* longest_safe_area_length, const char* current_location, const char* longest_safe_area)
 {
-     if (safe_area_length >= *longest_safe_area_length) {
+     if (safe_area_length > *longest_safe_area_length) {
          *longest_safe_area_length = safe_area_length;
          
          return current_location;
