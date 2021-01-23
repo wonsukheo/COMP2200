0680e9510f97b2d466bfa0ce51eddb29a058d21a
wonsukheo <62510983+wonsukheo@users.noreply.github.com>
2021-01-22 21:33:36 -0600

    coding style error fixed #1

--- Lab3/cyber_spaceship.c
+++ Lab3/cyber_spaceship.c
@@ -2,35 +2,35 @@
 
 void update_overlapped_count(const size_t cluster_count, const char* const cluster_start_locations[], const size_t cluster_lengths[], const char* current_location, size_t* overlapped_count)
 {
-     size_t j;
+    size_t j;
 
-     for (j = 0; j < cluster_count; ++j) {
-         if (current_location == cluster_start_locations[j]) {
+    for (j = 0; j < cluster_count; ++j) {
+        if (current_location == cluster_start_locations[j]) {
              *overlapped_count += 1;
-         } 
-         if (current_location == cluster_start_locations[j] + cluster_lengths[j]) {
+        } 
+        if (current_location == cluster_start_locations[j] + cluster_lengths[j]) {
              *overlapped_count -= 1;
-         }
-     } 
+        }
+    } 
 }
 
 const char* update_longest_safe_area_length(size_t safe_area_length, size_t* longest_safe_area_length, const char* current_location, const char* longest_safe_area)
 {
-     if (safe_area_length == 0 || longest_safe_area_length == 0) {
-         return longest_safe_area;
-     }
-     if (safe_area_length >= *longest_safe_area_length) {
-         *longest_safe_area_length = safe_area_length;
+            if (safe_area_length == 0 || longest_safe_area_length == 0) {
+                return longest_safe_area;
+            }
+            if (safe_area_length >= *longest_safe_area_length) {
+                *longest_safe_area_length = safe_area_length;
          
-         return current_location;
-     }
-     return longest_safe_area;
+                return current_location;
+            }
+            return longest_safe_area;
 }
             
 const char* get_longest_safe_zone_or_null(const char* const cab_start_location, const size_t cab_length, const char* const cluster_start_locations[], const size_t cluster_lengths[], const size_t cluster_count, size_t* out_longest_safe_area_length)
 {
-	    const char* current_location = cab_start_location; 
-            size_t safe_area_length = 0;
+    const char* current_location = cab_start_location; 
+    size_t safe_area_length = 0;
             size_t longest_safe_area_length = 0;
             const char* longest_safe_area = 0;
             size_t overlapped_count = 0;            
@@ -63,8 +63,8 @@ int get_travel_time(const char* const cab_start_location, const size_t cab_lengt
     const char* current_location = cab_start_location; 
     size_t overlapped_count = 0;
     double travel_time = 0;
-    const double safe_area_travel_speed = 0.1;
-    const double cluster_area_travel_speed = 0.2;
+    const double SAFE_AREA_TRAVEL_SPEED = 0.1;
+    const double CLUSTER_AREA_TRAVEL_SPEED = 0.2;
     size_t i;
 
     for (i = 0; i < cab_length; ++i) {
