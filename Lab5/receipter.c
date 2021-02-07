1eab7983f1ae98949085c938cc726269555bdbeb
wonsukheo <62510983+wonsukheo@users.noreply.github.com>
2021-02-07 00:40:26 -0600

    1st try

--- Lab5/receipter.c
+++ Lab5/receipter.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <time.h>
-#include "receipter.h"
 #include <string.h>
+#include "receipter.h"
 
 #define RECEIPT_WIDTH (51)
 
 static char g_spacing[51];
+
 char heading_buffer[256];
 char* heading_buffer_ptr = heading_buffer;
 char item_buffer[512];
 char* item_buffer_ptr = item_buffer;
-
 char tip_buffer[64];
 char* tip_buffer_ptr = tip_buffer;
-
 char total_buffer[512];
 char* total_buffer_ptr = total_buffer;
 char message_buffer[128];
@@ -29,12 +28,13 @@ size_t order_number = 0u;
 int add_item(const char* name, double price)
 { 
     int printed_item = 0;
+    
     if (item_count >= 10) {
         return FALSE;
     }
 
     printed_item = sprintf(item_buffer_ptr, "%33s %16.2f\n", name, price);
-    
+
     if (printed_item < 0) {
         return FALSE;
     } else {
@@ -47,7 +47,6 @@ int add_item(const char* name, double price)
 
 void set_tip(double tip)
 {
-
     if (tip != 0.00) {
         sprintf(tip_buffer_ptr, "%33s %16.2f\n", "Tip", tip);
         subtotal += tip;
@@ -60,7 +59,7 @@ void set_total(void)
 {
     size_t i;
     double tax = (subtotal * .05) + 0.005;
-    
+
     for (i = 0; i < RECEIPT_WIDTH - 1; ++i) {
         g_spacing[i] = ' ';
     }
@@ -70,7 +69,7 @@ void set_total(void)
     total_buffer_ptr += RECEIPT_WIDTH;
     sprintf(total_buffer_ptr, "%33s %16.2f\n", "Subtotal", subtotal);
     total_buffer_ptr += RECEIPT_WIDTH;
-    
+
     if (tip_buffer_ptr != NULL) {    
         sprintf(total_buffer_ptr, "%50s\n", tip_buffer);
         total_buffer_ptr += RECEIPT_WIDTH;
@@ -85,18 +84,11 @@ void set_total(void)
 
 void set_message(const char* message)
 {
-    /*size_t string_length = 0;
-    size_t i;
-
-    while (*message++ != '\0') {
-        string_length++;
-    }*/
-        
-    sprintf(message_buffer_ptr, "%-.50s\n", message);
+    sprintf(message_buffer_ptr, "%-50s\n", message);
     message_buffer_ptr += RECEIPT_WIDTH;
-    
+
     if (strlen(message) > RECEIPT_WIDTH - 1) {
-        sprintf(message_buffer_ptr, "%-.25s\n", message + RECEIPT_WIDTH - 1);
+        sprintf(message_buffer_ptr, "%-25s\n", message + RECEIPT_WIDTH - 1);
         message_buffer_ptr += 26;
     }
 }
@@ -118,8 +110,8 @@ void set_ending(void)
 
 void set_heading(time_t timestamp)
 {
-    char time_string[48];
     size_t i;
+    char time_string[48];
     int hours, minutes, seconds, day, month, year;
     
     struct tm* gm = gmtime(&timestamp);
@@ -127,21 +119,18 @@ void set_heading(time_t timestamp)
     hours = gm -> tm_hour;
     minutes = gm -> tm_min;
     seconds = gm -> tm_sec;
-   
+
     day = gm -> tm_mday;
     month = gm -> tm_mon + 1;
     year = gm -> tm_year + 1900;
-    
-    
-
 
     sprintf(time_string, "%d-%02d-%02d %02d:%02d:%02d", year, month, day, hours, minutes, seconds);
-    
+
     for (i = 0; i < RECEIPT_WIDTH - 1; ++i) {
         g_spacing[i] = '-';
     }
     g_spacing[i] = '\0';
-        
+
     sprintf(heading_buffer_ptr, "%-50s\n", "Charles' Seafood");
     heading_buffer_ptr += RECEIPT_WIDTH;
     sprintf(heading_buffer_ptr, "%50s\n", g_spacing);
@@ -155,14 +144,26 @@ void set_heading(time_t timestamp)
 int print_receipt(const char* filename, time_t timestamp)
 {
     FILE* stream;
-    
+
     set_heading(timestamp);
-    set_ending();
     set_total();
+    set_ending();
     
     if (item_buffer == item_buffer_ptr) {
+        order_number++;
+        item_count = 0u;
+        subtotal = 0.00;
+
+        heading_buffer_ptr = heading_buffer;
+        item_buffer_ptr = item_buffer;
+        total_buffer_ptr = total_buffer;
+        message_buffer_ptr = message_buffer;
+        ending_buffer_ptr = ending_buffer;
+        tip_buffer_ptr = tip_buffer;
+        
         return 0;
     } 
+    
     stream = fopen(filename, "w+");  
     fwrite(heading_buffer, sizeof(heading_buffer[0]), heading_buffer_ptr - heading_buffer, stream);
     fwrite(item_buffer, sizeof(item_buffer[0]), item_buffer_ptr - item_buffer, stream);
@@ -172,16 +173,17 @@ int print_receipt(const char* filename, time_t timestamp)
     fflush(stream);
     fclose(stream);
 
+    
     order_number++;
     item_count = 0u;
     subtotal = 0.00;
+
     heading_buffer_ptr = heading_buffer;
     item_buffer_ptr = item_buffer;
     total_buffer_ptr = total_buffer;
     message_buffer_ptr = message_buffer;
     ending_buffer_ptr = ending_buffer;
+    tip_buffer_ptr = tip_buffer;
 
     return 1;
 }
-
-
